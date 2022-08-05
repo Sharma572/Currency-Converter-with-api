07f442f0d017d988e2ff1b7d1b0bf79610dc0c5a
Raunak Sharma <raunakk572@gmail.com>
2022-08-05 13:24:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+ function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+  const url = "https://v6.exchangerate-api.com/v6/0821a9820552888a7ea9ba9c/latest/"
+ 
+   fetch(`${url}${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      const rate = data.conversion_rates[currency_two];
+      
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2)
+    })
+
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+// Swap btn Execution
+swap.addEventListener('click', ()=> {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate()
+})
+
+calculate();
